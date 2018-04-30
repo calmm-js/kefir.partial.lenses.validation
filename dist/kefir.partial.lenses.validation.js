f07c8bfa380416f7395f3672ff06d2df8672f362
Vesa Karvonen <vesa.a.j.k@gmail.com>
2018-04-30 10:44:08 +0300

    1.0.2

--- dist/kefir.partial.lenses.validation.js
+++ dist/kefir.partial.lenses.validation.js
@@ -1,55 +1,55 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('partial.lenses.validation'), require('kefir.combines')) :
-	typeof define === 'function' && define.amd ? define(['exports', 'partial.lenses.validation', 'kefir.combines'], factory) :
-	(factory((global.Kefir = global.Kefir || {}, global.Kefir.partial = global.Kefir.partial || {}, global.Kefir.partial.lenses = global.Kefir.partial.lenses || {}, global.Kefir.partial.lenses.validation = {}),global.partial.lenses.validation,global.kefir.combines));
-}(this, (function (exports,V,kefir_combines) { 'use strict';
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('partial.lenses.validation'), require('karet.lift')) :
+	typeof define === 'function' && define.amd ? define(['exports', 'partial.lenses.validation', 'karet.lift'], factory) :
+	(factory((global.Kefir = global.Kefir || {}, global.Kefir.partial = global.Kefir.partial || {}, global.Kefir.partial.lenses = global.Kefir.partial.lenses || {}, global.Kefir.partial.lenses.validation = {}),global.partial.lenses.validation,global.karet.lift));
+}(this, (function (exports,V,K) { 'use strict';
 
 	// THIS FILE IS GENERATED
 
-	var accept = /*#__PURE__*/kefir_combines.liftFOF(V.accept);
-	var acceptAs = /*#__PURE__*/kefir_combines.liftFOF(V.acceptAs);
-	var acceptWith = /*#__PURE__*/kefir_combines.liftFOF(V.acceptWith);
-	var accepts = /*#__PURE__*/kefir_combines.liftFOF(V.accepts);
-	var acceptsAsync = /*#__PURE__*/kefir_combines.liftFOF(V.acceptsAsync);
-	var and = /*#__PURE__*/kefir_combines.liftFOF(V.and);
-	var args = /*#__PURE__*/kefir_combines.liftFOF(V.args);
-	var arrayId = /*#__PURE__*/kefir_combines.liftFOF(V.arrayId);
-	var arrayIx = /*#__PURE__*/kefir_combines.liftFOF(V.arrayIx);
-	var both = /*#__PURE__*/kefir_combines.liftFOF(V.both);
-	var cases = /*#__PURE__*/kefir_combines.liftFOF(V.cases);
-	var casesOf = /*#__PURE__*/kefir_combines.liftFOF(V.casesOf);
-	var choose = /*#__PURE__*/kefir_combines.liftFOF(V.choose);
-	var dependentFn = /*#__PURE__*/kefir_combines.liftFOF(V.dependentFn);
-	var either = /*#__PURE__*/kefir_combines.liftFOF(V.either);
-	var errors = /*#__PURE__*/kefir_combines.liftFOF(V.errors);
-	var errorsAsync = /*#__PURE__*/kefir_combines.liftFOF(V.errorsAsync);
-	var freeFn = /*#__PURE__*/kefir_combines.liftFOF(V.freeFn);
-	var ifElse = /*#__PURE__*/kefir_combines.liftFOF(V.ifElse);
-	var keep = /*#__PURE__*/kefir_combines.liftFOF(V.keep);
-	var lazy = /*#__PURE__*/kefir_combines.liftFOF(V.lazy);
-	var modifyAfter = /*#__PURE__*/kefir_combines.liftFOF(V.modifyAfter);
-	var modifyError = /*#__PURE__*/kefir_combines.liftFOF(V.modifyError);
-	var not = /*#__PURE__*/kefir_combines.liftFOF(V.not);
-	var optional = /*#__PURE__*/kefir_combines.liftFOF(V.optional);
-	var or = /*#__PURE__*/kefir_combines.liftFOF(V.or);
-	var promote = /*#__PURE__*/kefir_combines.liftFOF(V.promote);
-	var props = /*#__PURE__*/kefir_combines.liftFOF(V.props);
-	var propsOr = /*#__PURE__*/kefir_combines.liftFOF(V.propsOr);
-	var reject = /*#__PURE__*/kefir_combines.liftFOF(V.reject);
-	var rejectAs = /*#__PURE__*/kefir_combines.liftFOF(V.rejectAs);
-	var rejectWith = /*#__PURE__*/kefir_combines.liftFOF(V.rejectWith);
-	var remove = /*#__PURE__*/kefir_combines.liftFOF(V.remove);
-	var removeAfter = /*#__PURE__*/kefir_combines.liftFOF(V.removeAfter);
-	var run = /*#__PURE__*/kefir_combines.liftFOF(V.run);
-	var setAfter = /*#__PURE__*/kefir_combines.liftFOF(V.setAfter);
-	var setError = /*#__PURE__*/kefir_combines.liftFOF(V.setError);
-	var tryValidateAsyncNow = /*#__PURE__*/kefir_combines.liftFOF(V.tryValidateAsyncNow);
-	var tuple = /*#__PURE__*/kefir_combines.liftFOF(V.tuple);
-	var upgrades = /*#__PURE__*/kefir_combines.liftFOF(V.upgrades);
-	var upgradesOf = /*#__PURE__*/kefir_combines.liftFOF(V.upgradesOf);
-	var validate = /*#__PURE__*/kefir_combines.liftFOF(V.validate);
-	var validateAsync = /*#__PURE__*/kefir_combines.liftFOF(V.validateAsync);
-	var where = /*#__PURE__*/kefir_combines.liftFOF(V.where);
+	var accept = /*#__PURE__*/K.lift(V.accept);
+	var acceptAs = /*#__PURE__*/K.lift(V.acceptAs);
+	var acceptWith = /*#__PURE__*/K.lift(V.acceptWith);
+	var accepts = /*#__PURE__*/K.lift(V.accepts);
+	var acceptsAsync = /*#__PURE__*/K.lift(V.acceptsAsync);
+	var and = /*#__PURE__*/K.lift(V.and);
+	var args = /*#__PURE__*/K.lift(V.args);
+	var arrayId = /*#__PURE__*/K.lift(V.arrayId);
+	var arrayIx = /*#__PURE__*/K.lift(V.arrayIx);
+	var both = /*#__PURE__*/K.lift(V.both);
+	var cases = /*#__PURE__*/K.lift(V.cases);
+	var casesOf = /*#__PURE__*/K.lift(V.casesOf);
+	var choose = /*#__PURE__*/K.lift(V.choose);
+	var dependentFn = /*#__PURE__*/K.lift(V.dependentFn);
+	var either = /*#__PURE__*/K.lift(V.either);
+	var errors = /*#__PURE__*/K.lift(V.errors);
+	var errorsAsync = /*#__PURE__*/K.lift(V.errorsAsync);
+	var freeFn = /*#__PURE__*/K.lift(V.freeFn);
+	var ifElse = /*#__PURE__*/K.lift(V.ifElse);
+	var keep = /*#__PURE__*/K.lift(V.keep);
+	var lazy = /*#__PURE__*/K.lift(V.lazy);
+	var modifyAfter = /*#__PURE__*/K.lift(V.modifyAfter);
+	var modifyError = /*#__PURE__*/K.lift(V.modifyError);
+	var not = /*#__PURE__*/K.lift(V.not);
+	var optional = /*#__PURE__*/K.lift(V.optional);
+	var or = /*#__PURE__*/K.lift(V.or);
+	var promote = /*#__PURE__*/K.lift(V.promote);
+	var props = /*#__PURE__*/K.lift(V.props);
+	var propsOr = /*#__PURE__*/K.lift(V.propsOr);
+	var reject = /*#__PURE__*/K.lift(V.reject);
+	var rejectAs = /*#__PURE__*/K.lift(V.rejectAs);
+	var rejectWith = /*#__PURE__*/K.lift(V.rejectWith);
+	var remove = /*#__PURE__*/K.lift(V.remove);
+	var removeAfter = /*#__PURE__*/K.lift(V.removeAfter);
+	var run = /*#__PURE__*/K.lift(V.run);
+	var setAfter = /*#__PURE__*/K.lift(V.setAfter);
+	var setError = /*#__PURE__*/K.lift(V.setError);
+	var tryValidateAsyncNow = /*#__PURE__*/K.lift(V.tryValidateAsyncNow);
+	var tuple = /*#__PURE__*/K.lift(V.tuple);
+	var upgrades = /*#__PURE__*/K.lift(V.upgrades);
+	var upgradesOf = /*#__PURE__*/K.lift(V.upgradesOf);
+	var validate = /*#__PURE__*/K.lift(V.validate);
+	var validateAsync = /*#__PURE__*/K.lift(V.validateAsync);
+	var where = /*#__PURE__*/K.lift(V.where);
 
 	exports.accept = accept;
 	exports.acceptAs = acceptAs;
